523072c94e050d616d68c415c8b2b86ea2cb87f9
ILYA OSIPOV <theoneadress@yandex.ru>
2025-12-19 20:32:11 +0300

    ‘version_s4 git checkout main git checkout main

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,38 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor {
+        guard let color = UIColor(named: "YP Black") else { // с пробелом!
+            fatalError("Не удалось загрузить цвет 'YP Black' из Assets")
+        }
+        return color
+    }
+    
+    static var ypRed: UIColor {
+        guard let color = UIColor(named: "YP Red") else { // с пробелом!
+            fatalError("Не удалось загрузить цвет 'YP Red' из Assets")
+        }
+        return color
+    }
+    
+    static var ypWhite: UIColor {
+        guard let color = UIColor(named: "YP White") else { // с пробелом!
+            fatalError("Не удалось загрузить цвет 'YP White' из Assets")
+        }
+        return color
+    }
+    
+    static var ypGreen: UIColor {
+        guard let color = UIColor(named: "YP Green") else { // с пробелом!
+            fatalError("Не удалось загрузить цвет 'YP Green' из Assets")
+        }
+        return color
+    }
+    
+    static var ypGray: UIColor {
+        guard let color = UIColor(named: "YP Gray") else { // с пробелом!
+            fatalError("Не удалось загрузить цвет 'YP Gray' из Assets")
+        }
+        return color
+    }
+}
